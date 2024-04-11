e1c434b41687221cc351c7c041e4829b7d0ab036
Matteo Tosato <matteo.tosato7@gmail.com>
2024-04-11 18:24:50 +0200

    ..start with a source generator sample projects

---
+++ SourceGeneratorSample/SampleGenerator.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SourceGeneratorSample
+{
+    public class SampleGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var compilation = context.Compilation; // Get the supplier user code
+            var syntaxTrees = compilation.SyntaxTrees;
+
+            foreach (var syntaxTree in syntaxTrees)
+            {
+                var model = compilation.GetSemanticModel(syntaxTree);
+                
+                var classes = syntaxTree.GetRoot()
+                    .DescendantNodes()
+                    .OfType<ClassDeclarationSyntax>();
+
+                foreach (var @class in classes)
+                {
+                    var classSymbol = model.GetDeclaredSymbol(@class);
+
+                    context.AddSource(
+                        $"{classSymbol.Name}_HelloWorld.cs",
+                        SourceText.From($$"""
+
+                                          namespace {{classSymbol.ContainingNamespace}}
+                                          {
+                                              public partial class {{@class.Identifier}}
+                                              {
+                                                  public void HelloWorld()
+                                                  {
+                                                      System.Console.WriteLine("Hello, World!");
+                                                  }
+                                              }
+                                          }
+
+                                          """, Encoding.UTF8));
+                }
+            }
+        }
+    }
+}
