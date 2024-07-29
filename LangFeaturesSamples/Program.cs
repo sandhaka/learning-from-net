513a82bf56e2456925bc79ed4d856122af00e78b
Matteo Tosato <matteo.tosato7@gmail.com>
2024-07-29 10:57:07 +0200

    Add a language features sample project

---
+++ LangFeaturesSamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+/*
+ * Using array manipulations
+ */
+
+Console.WriteLine("Array manipulations:");
+
+string[] array = ["A", "B", "C"];
+Console.WriteLine(JsonSerializer.Serialize(array));
+
+Console.WriteLine("Get last element from an array and remove it");
+
+var last = array.Last();
+array = array[..^1];
+
+Console.WriteLine($"Last Element: {last}");
+Console.WriteLine($"Remains: {JsonSerializer.Serialize(array)}");
+
+Console.WriteLine("Get first element from an array and remove it");
+
+var first = array.First();
+array = array[1..];
+
+Console.WriteLine($"First Element: {first}");
+Console.WriteLine($"Remains: {JsonSerializer.Serialize(array)}");
