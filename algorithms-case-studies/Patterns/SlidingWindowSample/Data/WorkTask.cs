03c6f5dcf2524c2fb1fad9679e6ee1641b2b6244
Matteo Tosato <matteo.tosato7@gmail.com>
2025-03-24 17:39:18 +0100

    Sliding Window Pattern - Sample use case data generator

---
+++ algorithms-case-studies/Patterns/SlidingWindowSample/Data/WorkTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingWindowSample.Data
+{
+    /// <summary>
+    /// Work Assessment data
+    /// </summary>
+    /// <param name="Effort">Estimation, assessment value</param>
+    /// <param name="Deviation">Assessment deviation percentage</param>
+    /// <param name="Value">Impact, value of the work. Higher has more value when accomplished</param>
+    public record struct WorkTask(int Effort, double Deviation, double Value);
+}
---
+++ algorithms-case-studies/Patterns/SlidingWindowSample/SW/SlidingWindowFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingWindowSample.SW
+{
+    public static class SlidingWindowFactory
+    {
+        public static ISlidingWindow Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
