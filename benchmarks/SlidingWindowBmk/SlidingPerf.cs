5fbf123bb1087bfc736430f99a736fffc841974b
Matteo Tosato <matteo.tosato7@gmail.com>
2025-04-01 18:10:15 +0200

    Benchmarking

--- benchmarks/SlidingWindowBmk/SlidingPerf.cs
+++ benchmarks/SlidingWindowBmk/SlidingPerf.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using SlidingWindowSample.SW;
 
@@ -13,30 +8,62 @@ namespace SlidingWindowBmk
     [SimpleJob(RuntimeMoniker.Net80)]
     public class SlidingPerf
     {
-        [Params(10, 100, 1000)] private int _windowSize;
-        [Params(1000, 10000, 100000, 1000000)] private int _length;
+        [Params(10, 100)] public int WindowSize { get; set; }
+        [Params(100000, 1000000)] public int Length { get; set; }
 
-        private readonly IEnumerable<int> _enumerable = Enumerable.Range(0, 1000000);
+        private readonly IEnumerable<int> _enumerable = Enumerable.Range(0, 1000001);
         private readonly int[] _arr = Enumerable.Range(0, 1000000).ToArray();
-        private readonly ISlidingWindow<int> _memorySlidingWindow;
-        private readonly ISlidingWindow<int> _queueSlidingWindow;
+        private readonly IAccumulator<int> _accumulator = new SimpleAccumulator();
 
-        public SlidingPerf()
+        private ISlidingWindow<int> _memorySlidingWindow;
+        private ISlidingWindow<int> _queueSlidingWindow;
+
+        private class SimpleAccumulator : IAccumulator<int>
         {
-            _memorySlidingWindow = SlidingWindowFactory.Create(_arr, 0, _windowSize);
-            _queueSlidingWindow = SlidingWindowFactory.Create(_enumerable, 0, _windowSize);
+            public int CompareTo(object? obj)
+            {
+                if (obj is not SimpleAccumulator wtaObj)
+                    return 1;
+                if (wtaObj.Value > this.Value)
+                    return -1;
+                if (wtaObj.Value < this.Value)
+                    return 1;
+                return 0;
+            }
+
+            public bool Equals(IAccumulator<int>? other)
+            {
+                if (other == null) return false;
+                if (ReferenceEquals(this, other))
+                    return true;
+                return other.Value == Value;
+            }
+
+            public int Value { get; private set; } = 0;
+            public void Process(int current) => Value += current;
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            _memorySlidingWindow = SlidingWindowFactory.Create(_arr, 0, WindowSize);
+            _queueSlidingWindow = SlidingWindowFactory.Create(_enumerable, 0, WindowSize);
+            _memorySlidingWindow.AddAccumulator(_accumulator);
+            _queueSlidingWindow.AddAccumulator(_accumulator);
         }
 
         [Benchmark(Description = "Memory<> implementation")]
         public void MemorySlidingWindow()
         {
-
+            for (var i = 0; i < Length - WindowSize; i++)
+                _memorySlidingWindow.Advance(1);
         }
 
         [Benchmark(Description = "Queue<> implementation")]
         public void QueueSlidingWindow()
         {
-
+            for (var i = 0; i < Length - WindowSize; i++)
+                _queueSlidingWindow.Advance(1);
         }
     }
 }
